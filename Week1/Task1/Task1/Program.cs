5513536a0f8856cb1a7b6d5c0b46224f1aa5268a
Assel <alieva.a2703@gmail.com>
2019-01-28 23:23:56 +0600

    w1t1

---
+++ Week1/Task1/Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            string[] s = Console.ReadLine().Split();
+            for (int i = 0; i < n; i++)
+            {
+                int x;
+                a[i] = Convert.ToInt32(s[i]);
+            }
+            int cnt = 1;
+            int num = 0;
+            int[] b = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = 2; j < a[i]; j++)
+                {
+                    if (a[i] % j == 0)
+                    {
+                        cnt++;
+                    }
+                }
+                if (cnt < 2 && a[i] != 1)
+                {
+                    b[num] = a[i];
+                    num++;
+                }
+                cnt = 1;
+            }
+            Console.WriteLine(num);
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write(b[i] + " ");
+            }
+
+        }
+    }
+}
---
+++ Week1/Task3/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            string[] s = Console.ReadLine().Split();
+            for (int i = 0; i < n; i++)
+            {
+                int x;
+                a[i] = Convert.ToInt32(s[i]);
+            }
+            for (int i = 0; i < a.Length; ++i)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(a[i] + " ");
+                }
+            }
+        }
+    }
+}
