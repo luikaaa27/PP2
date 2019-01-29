f2b6a70d5ac8554686be6fd247712f0ad78da47d
Assel <alieva.a2703@gmail.com>
2019-01-29 17:03:33 +0600

    w1t1

--- Week1/Task3/Task3/Program.cs
+++ Week1/Task3/Task3/Program.cs
@@ -11,19 +11,18 @@ namespace Task3
         static void Main(string[] args)
         {
             int n;
-            n = Convert.ToInt32(Console.ReadLine());
-            int[] a = new int[n];
-            string[] s = Console.ReadLine().Split();
+            n = Convert.ToInt32(Console.ReadLine());//convert from string to int
+            int[] a = new int[n];//initialize an array
+            string[] s = Console.ReadLine().Split();//Read the line and split to string array
             for (int i = 0; i < n; i++)
-            {
-                int x;
-                a[i] = Convert.ToInt32(s[i]);
+            { 
+                a[i] = Convert.ToInt32(s[i]);//convert elements of array from string to int
             }
             for (int i = 0; i < a.Length; ++i)
             {
                 for (int j = 0; j < 2; j++)
                 {
-                    Console.Write(a[i] + " ");
+                    Console.Write(a[i] + " ");//output an another array of integers, where every element is repeated
                 }
             }
         }
