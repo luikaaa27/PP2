cdc04d0192a8ed2ba2c538f95df0d0a58a45ddaf
Assel <alieva.a2703@gmail.com>
2019-01-29 17:27:57 +0600

    w1t1

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());//Convert from string to int
+            int[] a = new int[n];//initialiaze an array
+            for(int i=1; i <= n; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("[*]");//output the Star triangle
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
