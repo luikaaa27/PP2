eefd5d3d9f9f8014898b2ef24271bb00e8840840
Assel <alieva.a2703@gmail.com>
2019-02-04 17:37:23 +0600

    w2t4

---
+++ Week2/Task4/Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\asus-\source\repos\Week2\Task4\task4Path.txt";
+            string path1 = @"C:\Users\asus-\source\repos\Week2\Task4\task4PathNew.txt";
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                fileInf.CopyTo(path1, true);
+                fileInf.Delete();
+            }
+        }
+    }
+}
