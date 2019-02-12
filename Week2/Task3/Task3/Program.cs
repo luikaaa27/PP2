5822aa89db64e6c669166c0fdcb5601df615f485
Assel <alieva.a2703@gmail.com>
2019-02-12 22:53:44 +0600

    W2T3

---
+++ Week2/Task3/Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Task3
+{
+    class Program
+    {
+        static void Tree(DirectoryInfo root, string level)
+        {
+            FileInfo[] files = null;
+            DirectoryInfo[] subDir = null;
+            files = root.GetFiles("*.*");
+           
+            if (files != null)
+            {
+                subDir = root.GetDirectories();
+                foreach (DirectoryInfo dirInfo in subDir)
+                {
+                    Console.WriteLine(level + dirInfo.Name);
+                    Tree(dirInfo, level + "   ");
+                }
+                foreach (FileInfo fi in files)
+                {
+                    Console.WriteLine(level + fi.Name);
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\asus-\source\repos");
+            Tree(dirInfo, "");
+        }
+    }
+}
